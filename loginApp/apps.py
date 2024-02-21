0bab2b93ad77ccb31a6e0e7263633ed8a21a2631
ruizhang <kwf8hq@virginia.edu>
2024-02-21 16:57:50 -0500

    oauth refactor

--- loginApp/apps.py
+++ loginApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Proj28Config(AppConfig):
+class loginAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'loginApp'
