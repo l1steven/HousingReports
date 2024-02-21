fd2c49b45afd88d8afb279b5afd842b47d8bd30e
ruizhang <kwf8hq@virginia.edu>
2024-02-21 17:45:12 -0500

    change loginApp folder to match templating

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="loginApp/index.html")),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
 ]
