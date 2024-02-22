01c715523aaa887e26e5e3f8857044514897c12c
kevinthisyear <lam.kevin2304@gmail.com>
2024-02-22 13:25:30 -0500

    added separate dashborads for user and admin

--- loginApp/views.py
+++ loginApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+    if request.user.is_superuser:
+        return render(request, 'AdminDashboard.html')
+    else:
+        return render(request, 'user_dashboard.html')
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
+from loginApp import views as loginAppView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="loginApp/index.html")),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
+    path('dashboard/', loginAppView.dashboard, name='dashboard'),
 ]
