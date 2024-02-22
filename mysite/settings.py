4c51488c62b72f16ec5418f93da8c13bc777c75c
Steven Li <steven.baobao@gmail.com>
2024-02-21 20:36:52 -0500

    updated requirements and settings for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import secrets
 from pathlib import Path
 from django.core.checks import templates
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s=iz$4sc1)2v00@cm^j8))ga#o68_4%7uo4$mg8((y!chxj7xs'
+#SECRET_KEY = 'django-insecure-s=iz$4sc1)2v00@cm^j8))ga#o68_4%7uo4$mg8((y!chxj7xs'
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    default=secrets.token_urlsafe(nbytes=64),
+)
+# SECURITY WARNING: don't run with debug turned on in production!
+
+IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if not IS_HEROKU_APP:
+    DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -151,3 +160,5 @@ SITE_ID = 2
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+import django_heroku
+django_heroku.settings(locals())
