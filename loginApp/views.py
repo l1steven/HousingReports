feb97be922525b05cc2a9d0550344628d9f3aca6
ruizhang <kwf8hq@virginia.edu>
2024-02-26 12:59:02 -0500

    group name

--- loginApp/views.py
+++ loginApp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def dashboard(request):
-    if request.user.groups.filter(name = 'whateveradmingroupnameis').exists():
+    if request.user.groups.filter(name = 'Site Admin').exists():
         return render(request, 'loginApp/AdminDashboard.html')
     else:
         return render(request, 'loginApp/UserDashboard.html')
