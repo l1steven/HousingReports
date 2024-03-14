e2f0bbfc19edbeb932a05d4c22709fda9b82eb71
kevinthisyear <lam.kevin2304@gmail.com>
2024-03-14 13:39:06 -0400

    test if keys are pushed into repo

--- mysite/settings.py
+++ mysite/settings.py
@@ -178,3 +178,22 @@ django_heroku.settings(locals(),databases=False)
 # if 'DATABASE_URL' in os.environ:
 #     import dj_database_url
 #     DATABASES = {'default': dj_database_url.config()}
+
+# AWS S3 Settings
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', 'test')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', 'test2')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'test3')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_LOCATION = 'static'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media files
+# Note: Set MEDIA_URL and DEFAULT_FILE_STORAGE to direct Django to use S3 for media files
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
