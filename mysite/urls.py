64b51fd6445fcfd640e4d5493b4fe009a41d8f85
kevinthisyear <lam.kevin2304@gmail.com>
2024-03-15 20:29:53 -0400

    fixed urls error

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,7 +27,5 @@ urlpatterns = [
     path('logout', LogoutView.as_view(), name="logout"),
     path('dashboard/', loginAppView.dashboard, name='dashboard'),
     path('complaint/', views.complaint_form, name='complaint_form'),
-    path('submit_complaint/', views.submit_complaint, name='submit_complaint'),
-    path('complaint/', views.complaint_form, name='complaint_form'),
     path('complaint_success/', views.complaint_success, name='complaint_success'),
 ]
