950330e4fed71a3cee71f7160815a451c1920f44
kevinthisyear <lam.kevin2304@gmail.com>
2024-03-16 22:24:29 -0400

    made pdf and text files viewable

--- loginApp/views.py
+++ loginApp/views.py
@@ -1,3 +1,5 @@
+import mimetypes
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from loginApp.models import Complaint
@@ -7,6 +9,15 @@ from loginApp.forms import ComplaintForm
 @login_required
 def dashboard(request):
     complaints = Complaint.objects.filter(user=request.user) if not request.user.is_superuser else Complaint.objects.all()
+
+    # identify file type
+    for complaint in complaints:
+        if complaint.upload:
+            mime_type, _ = mimetypes.guess_type(complaint.upload.path)
+            complaint.is_image = mime_type.startswith('image/') if mime_type else False
+            complaint.is_pdf = 'application/pdf' == mime_type if mime_type else False
+            complaint.is_text = 'text/plain' == mime_type if mime_type else False
+
     userType = 'loginApp/AdminDashboard.html' if request.user.is_superuser else 'loginApp/UserDashboard.html'
     return render(request, userType, {'complaints': complaints})
 
