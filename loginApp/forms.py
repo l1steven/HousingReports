103fb83507128a117127dde681231c24532d9f47
ruizhang <kwf8hq@virginia.edu>
2024-04-23 21:25:16 -0400

    again

--- loginApp/forms.py
+++ loginApp/forms.py
@@ -1,12 +1,28 @@
 from django import forms
-
 from loginApp.models import Complaint
+from django.forms import ClearableFileInput
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
 
 class ComplaintForm(forms.ModelForm):
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
     class Meta:
         model = Complaint
         fields = ['name', 'location', 'description', 'upload']
+        upload = MultipleFileField(label='Select files', required=False)
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'location': forms.TextInput(attrs={'class': 'form-control'}),
@@ -15,8 +31,16 @@ class ComplaintForm(forms.ModelForm):
 
 class AnonymousComplaintForm(forms.ModelForm):
 
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+    
     def __init__(self, *args, **kwargs):
-        super(AnonymousComplaintForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.fields['name'].initial = 'Anonymous'
         self.fields['name'].widget.attrs['readonly'] = True
         self.fields['is_anonymous'].initial = True
@@ -25,8 +49,9 @@ class AnonymousComplaintForm(forms.ModelForm):
     class Meta:
         model = Complaint
         fields = ['name', 'location', 'description', 'upload', 'is_anonymous']
+        upload = MultipleFileField(label='Select files', required=False)
+
         widgets = {
             'location': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
-            'upload': forms.FileInput(attrs={'class': 'form-control'}),
         }
